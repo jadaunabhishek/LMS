86dc42d76babc50f6cd8905f935f4f2651e26cc1
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-22 10:59:47 +0530

    initial commit

---
+++ Library Management System/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Library Management System
+//
+//  Created by Abhishek Jadaun on 22/04/24.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
---
+++ Library Management System/Views/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  Library Management System
+//
+//  Created by Abhishek Jadaun on 22/04/24.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
