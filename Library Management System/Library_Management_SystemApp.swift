d633c77d464ab14ee50614af42f40028a7bd419a
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-22 09:40:36 +0530

    Initial Commit

---
+++ Library Management System/Library_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Library_Management_SystemApp.swift
+//  Library Management System
+//
+//  Created by Abhishek Jadaun on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Library_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
