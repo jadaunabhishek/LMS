69ac52c24db5f142c8febffba43842b937873ab4
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-22 11:45:09 +0530

    updated

---
+++ Library Management System/Views/Admin/FirstScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScreenView.swift
+//  Library Management System
+//
+//  Created by Abhishek Jadaun on 22/04/24.
+//
+
+import SwiftUI
+
+struct FirstScreenView: View {
+    var body: some View {
+        TabView(){
+            
+        }
+    }
+}
+
+#Preview {
+    FirstScreenView()
+}
