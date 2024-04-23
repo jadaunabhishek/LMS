74233de34f8f60403a2b5a32bd7c06c6474c0ce1
Vishal Tyagi <vishaltyagi.dev098@gmail.com>
2024-04-23 16:02:40 +0530

    card list detail view

--- Library Management System/Admin/Views/AdminCategories/Components/CardListDetailView.swift
+++ Library Management System/Admin/Views/AdminCategories/Components/CardListDetailView.swift
@@ -11,14 +11,34 @@ struct CardListDetailView: View {
     var books: [Bok]
     var body: some View {
         ForEach(books) { book in
-            ZStack{
-                Rectangle()
-                    .fill(.blue)
-                    .frame(maxWidth: .infinity).frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                Text(book.name)
-                
-                
+            HStack {
+                // Book cover image
+                Image(systemName: "book.fill") // Placeholder image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100) // Adjust size as needed
+                    .foregroundColor(.white)
+                    .padding()// Placeholder color
+                Spacer()
+                // VStack for book details
+                VStack(alignment: .leading) {
+                    // Book name
+                    Text(book.name)
+                        .font(.headline)
+                    
+                    // Book author
+                    Text("By \(book.author)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                .padding(.leading, 8) // Add padding between image and text
+                .padding(.trailing, 16) // Add trailing padding to align with the screen edge
             }
+            .frame(maxWidth: .infinity) // Expand to the entire screen width
+            .frame(height: 100) // Fixed height for the card
+            .background(Color.black.opacity(0.2)) // Background color for the card
+            .cornerRadius(10) // Apply corner radius for rounded corners
+            .padding(8) // Add padding around the card
         }
     }
 }
