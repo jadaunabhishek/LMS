60f61e2590ec774ed3d9a342e9091b6b76352e64
Manvi Singhal <manvi.singhal03@gmail.com>
2024-04-23 11:53:54 +0530

    set custom theme

--- Library Management System/Admin/Views/AdminHomeView.swift
+++ Library Management System/Admin/Views/AdminHomeView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct AdminHomeView: View {
+    @EnvironmentObject var themeManager: ThemeManager
+
     var body: some View {
-        Text("home view")
+        VStack {
+            Text("Select Theme")
+                .font(.headline)
+                .padding()
+
+            HStack{
+                ForEach(themeManager.themes, id: \.primaryThemeColor) { theme in
+                    Button(action: {
+                        themeManager.setTheme(theme)
+                    }) {
+                        ThemeView(theme: theme)
+                    }
+                }
+            }
+            
+            Text("Selected Theme:")
+                .font(.headline)
+                .padding()
+            ThemeView(theme: themeManager.selectedTheme)
+        }
+        .padding()
     }
 }
 
-#Preview {
-    AdminHomeView()
+struct ThemeView: View {
+    let theme: ThemeProtocol
+    
+    var body: some View {
+            Circle()
+                .fill(theme.primaryThemeColor)
+                .frame(width: 40, height: 40)
+                .padding(.trailing)
+    }
+}
+
+struct AdminHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        let themeManager = ThemeManager()
+        return AdminHomeView()
+            .environmentObject(themeManager)
+    }
 }
--- Library Management System/Colors/ThemeProtocol.swift
+++ Library Management System/Colors/ThemeProtocol.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 protocol ThemeProtocol {
     var primaryThemeColor: Color { get }
-    var secondoryThemeColor: Color { get }
+    var secondaryThemeColor: Color { get }
     //var affirmBtnTitleColor: Color { get }
     //var negativeBtnTitleColor: Color { get }
     var bodyTextColor: Color { get }
