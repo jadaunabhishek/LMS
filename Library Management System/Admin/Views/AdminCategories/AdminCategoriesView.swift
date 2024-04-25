1490593ba2a554c5488fb53b661cbfd294b64dd2
ManishNMR <2016nmr2016@gmail.com>
2024-04-25 11:06:07 +0530

    :sparkles: Search view update in Admin

--- Library Management System/Admin/Views/AdminCategories/AdminCategoriesView.swift
+++ Library Management System/Admin/Views/AdminCategories/AdminCategoriesView.swift
@@ -12,9 +12,15 @@ struct AdminCategoriesView: View {
     @State private var searchKey = ""
     
     @State var isPageLoading: Bool = true
-    
+    var filteredCategories: [String] {
+        if searchKey.isEmpty {
+            return configViewModel.currentConfig[0].categories
+        } else {
+            return configViewModel.currentConfig[0].categories.filter { $0.localizedCaseInsensitiveContains(searchKey) }
+        }
+    }
     var body: some View {
-    
+        
         NavigationStack {
             VStack(){
                 if(!isPageLoading){
@@ -23,36 +29,30 @@ struct AdminCategoriesView: View {
                         .frame(height: 80)
                         .cornerRadius(5)
                         .padding(.bottom)
-//                    TextField("what are u looking for?", text: $searchKey)
-//                        .foregroundStyle(.black)
-//                        
-//                        .padding(14)
-//                        .background{
-//                            RoundedRectangle(cornerRadius: 12)
-//                                .stroke(Color.black, lineWidth: 1)
-//                            
-//                        }
-//                    .padding(.horizontal,34)
-//                    .padding(.top,20)
-//                    Image(systemName: "magnifyingglass")
-//                        .offset(x:130,y:-35)
+                    TextField("what are u looking for?", text: $searchKey)
+                        .foregroundStyle(.black)
+                    
+                        .padding(14)
+                        .background{
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.black, lineWidth: 1)
+                            
+                        }
+                        .padding(.horizontal,34)
+                        .padding(.top,20)
+                    Image(systemName: "magnifyingglass")
+                        .offset(x:130,y:-35)
                     Spacer()
                     
-    //                let groupedBooks = Dictionary(grouping: books, by: { $0.category })
-                                
-                    ScrollView{
+                    ScrollView {
                         LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
-                            if(!configViewModel.currentConfig[0].categories.isEmpty){
-                                ForEach(configViewModel.currentConfig[0].categories, id: \.self) { category in
-                                    NavigationLink(destination:AdminSubCategoriesView(category: category)) {
-                                        VStack(alignment: .leading) {
-                                            AdminCategoriesCard(category: category)
-                                                .foregroundStyle(.black)
-                                        }
-                                        .padding(.horizontal)
+                            ForEach(filteredCategories, id: \.self) { category in
+                                NavigationLink(destination: AdminSubCategoriesView(category: category)) {
+                                    VStack(alignment: .leading) {
+                                        AdminCategoriesCard(category: category)
+                                            .foregroundStyle(.black)
                                     }
-        //
-        //                            .padding(.horizontal)
+                                    .padding(.horizontal)
                                 }
                             }
                         }
@@ -90,18 +90,5 @@ struct ACPre: View {
 
 #Preview {
     ACPre()
-        
-}
-
-
-struct Bok: Identifiable {
-    let id: Int
-    let isbn: String
-    let name: String
-    let author: String
-    let description: String
-    let publishingDate: String
-    let category: String
-    var subcategory: String
-    let status: String
+    
 }
