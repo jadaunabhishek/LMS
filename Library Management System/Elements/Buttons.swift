6dbd59a8bd7ef2999214e43bebee7cdea7965b57
Vishal Tyagi <vishaltyagi.dev098@gmail.com>
2024-05-08 11:42:05 +0530

    onboarding working

--- Library Management System/Elements/Buttons.swift
+++ Library Management System/Elements/Buttons.swift
@@ -20,6 +20,7 @@ struct PrimaryCustomButton: View {
                 .padding()
                 .background(themeManager.selectedTheme.primaryThemeColor)
                 .cornerRadius(15)
+                .foregroundColor(.white)
         }
         .buttonStyle(PlainButtonStyle())
     }
@@ -60,13 +61,16 @@ struct SignupCustomButton: View {
 
                 Text(label)
                     .font(.title3)
-                    .fontWeight(.bold)
                     .foregroundColor(themeManager.selectedTheme.bodyTextColor)
             }
             .frame(maxWidth: .infinity)
             .padding()
-            .background(Color(.systemGray4))
+            .background(Color(.white))
             .cornerRadius(15)
+            .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.black, lineWidth: 1) // Add a black border with width 1
+                )
         }
         .buttonStyle(PlainButtonStyle())
     }
--- Library Management System/Onboarding/OnboardingView.swift
+++ Library Management System/Onboarding/OnboardingView.swift
@@ -1,18 +1,74 @@
-//
-//  OnboardingView.swift
-//  Library Management System
-//
-//  Created by admin on 08/05/24.
-//
-
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var currentPage = 0
+    
+    let texts = ["We're thrilled to have you join our library community.", "Easily check out books, manage loans, and prebook items—all at your fingertips.", " From bestsellers to academic journals, we've got it all."]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TabView(selection: $currentPage) {
+                ForEach(0..<3) { index in // Use integer literals directly
+                    VStack {
+                        Image("onboardingImage\(index + 1)")
+                            .resizable()
+                            .scaledToFit()
+                            .tag(index)
+                        
+                        Text(texts[index])
+                            .padding(.top, 10)
+                    }
+                }
+            }
+            .frame(height: UIScreen.main.bounds.height / 2)
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            
+//            VStack {
+//                PrimaryCustomButton(action: {
+//                    // Action for "Agree & Join" button
+//                }, label: "Get Started")
+//            }.padding()
+            VStack {
+                PrimaryCustomButton(action: {
+                    // Action for "Agree & Join" button
+                }, label: "Agree & Join")
+                
+                
+                SignupCustomButton(action: {
+                    print("Login Attempt")
+                    
+                }, label: "Sign Up with Google",imageName: "google")
+                
+                
+                Button(action: {
+                    // Action for "Sign In" button
+                }) {
+                    Text("Sign In")
+                        .font(.title3)
+                        .padding()
+                        .foregroundColor(.blue)
+                }
+            }
+            .padding()
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    private func startTimer() {
+        _ = Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { timer in
+            withAnimation {
+                currentPage = (currentPage + 1) % 3
+            }
+        }
     }
 }
 
-#Preview {
-    OnboardingView()
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        let themeManager = ThemeManager()
+        OnboardingView()
+            .environmentObject(themeManager)
+    }
 }
