c65059f451634e73f16336fc5df3e94289e20f64
Rishichaary <rs3715@srmist.edu.in>
2024-05-09 10:41:09 +0530

    Yes

---
+++ Library Management System/Member/Views/MemberTabs/Review.swift
@@ -0,0 +1,18 @@
+//
+//  Review.swift
+//  Library Management System
+//
+//  Created by Rishichaary S on 08/05/24.
+//
+
+import SwiftUI
+
+struct Review: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Review()
+}
